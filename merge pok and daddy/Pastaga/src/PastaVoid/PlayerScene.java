0bd0eb74fc5e93adff91c47028dbc1e35ccf37e0
pachoulie <souy.angelina@gmail.com>
2013-12-12 11:35:59 +0900

    Add Cube Player

--- merge pok and daddy/Pastaga/src/PastaVoid/PlayerScene.java
+++ merge pok and daddy/Pastaga/src/PastaVoid/PlayerScene.java
@@ -7,17 +7,17 @@ public class PlayerScene extends AScene {
 
 	float  _posX;
 	float  _scale = 1.f;
-	float  _sizeX = 10.f;
-	float  _sizeY = 0.5f;
-	float  _speed = 1.f;
+	float  _sizeX = 0.075f;
+	float  _speed = 0.007f;
 	int	   _widthScreen;
+	float  _yDeg = 45.f;
 	LevelScene	_levelScene;
 	  
 	public PlayerScene(Game game, int widthScreen, LevelScene levelScene) {
 		super(game, true, true);
 		// TODO Auto-generated constructor stub
 		_widthScreen = widthScreen;
-	    _posX = 50;
+	    _posX = 0.5f;
 	    _levelScene = levelScene;
 	}
 
@@ -29,21 +29,31 @@ public class PlayerScene extends AScene {
 
 	@Override
 	public void update(long elapsedTime) {
+		_yDeg+=2.f;
 		if (KeysManager.getInstance().keyIsPressed(KeysManager.EKeys.LEFT) == true) {
-			if (_posX > _sizeX/2 + 1.f)
+			if (_posX > _sizeX/2)
 				_posX -= _speed;
 		}
 		else if (KeysManager.getInstance().keyIsPressed(KeysManager.EKeys.RIGHT) == true) {
-			if (_posX < 100 - _sizeX/2  - 1.f)
+			if (_posX < 1.f - _sizeX/2)
 			_posX += _speed;
 		}
-			
-		
 	}
 
 	@Override
 	public void draw(Game parent) {
-		parent.ellipse(_posX, _levelScene.getCamera().getOffset() + 0.5f, _sizeX * _scale, _sizeY * _scale);
+		//parent.ellipse(_posX, _levelScene.getCamera().getOffset() + 0.5f, _sizeX * _scale, _sizeY * _scale);
+		parent.pushMatrix();
+		parent.stroke(255);
+		parent.noFill();
+		parent.translate(_posX, _levelScene.getCamera().getOffset() + 0.5f, 0.f);
+		
+		
+		parent.rotateX(parent.radians(67.f));
+		parent.rotateY(parent.radians(_yDeg));
+		
+		parent.box(_sizeX, _sizeX * 4f, _sizeX);
+		parent.popMatrix();
 	}
 
 }
