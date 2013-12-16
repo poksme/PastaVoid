3f88e20ca07a66b10b49ca5eff046f844ed69c67
Bertrand Boustany <bertrand@boustany.me>
2013-12-16 22:07:53 +0900

    Start Menu done

--- merge pok and daddy/Pastaga/src/PastaVoid/MenuScene.java
+++ merge pok and daddy/Pastaga/src/PastaVoid/MenuScene.java
@@ -2,6 +2,7 @@ package PastaVoid;
 
 import java.util.ArrayList;
 
+import processing.core.PImage;
 import Configuration.Config;
 import GameEngine.AScene;
 import GameEngine.KeysManager;
@@ -14,12 +15,14 @@ public class MenuScene extends AScene {
     private ArrayList<SongEntry> songLibrary;
     private float totalElapsedTime;
     private float posZ;
+    private PImage logo;
 	
 	public MenuScene(Game game, Config config) {
     	super(game, true, true); // UPDTABLE AND DRAWABLE
     	curSong = 0;
     	totalElapsedTime = 0;
     	posZ = 0;
+    	logo = game.loadImage("TitleLogo.jpg");
     	songLibrary = new ArrayList<SongEntry>();
     	for (int i = 0; i < config.getLevels().size(); i++) {
     		songLibrary.add(new SongEntry(config.getLevels().get(i).getArtist() + "\n" + config.getLevels().get(i).getName()));
@@ -55,9 +58,33 @@ public class MenuScene extends AScene {
 
 	public void draw(Game parent) {
 		parent.camera();
-		parent.fill(255);
-//        parent.fill(0, 102, 153);
-        parent.textAlign(parent.CENTER);
-        parent.text(songLibrary.get(curSong).getTitle(), parent.width / 2, parent.height / 2 - 48, posZ);		
+	      parent.textAlign(parent.CENTER);
+
+//	      parent.fill(48, 117, 232);
+////	      parent.fill(30, 73, 145, 100);
+//	      parent.strokeWeight(0.5f);
+//	      parent.text("PASTAVOID", parent.width / 2, parent.height / 2 - 100, 200);      
+//      parent.blur();
+//
+////      parent.fill(30, 73, 145, 100);
+//      parent.fill(48, 117, 232);
+//      parent.strokeWeight(0.05f);
+//      parent.text("PASTAVOID", parent.width / 2, parent.height / 2 - 100, 200);
+//      parent.blur();
+
+	      parent.image(logo, parent.width / 2 - 340, 60);
+      parent.fill(255);
+      parent.text(songLibrary.get(curSong).getTitle(), parent.width / 2, parent.height / 2 , posZ);
+      int trSize = 30;
+
+      int trY = parent.height / 2;
+      int trX = 140;
+      parent.triangle(trX + 0, trY + trSize / 2, trX + trSize / 2, trY + 0, trX + trSize / 2 , trY + trSize);
+
+      trX = parent.width - 140 - trSize / 2;
+      parent.triangle(trX + 0, trY + 0, trX + 0, trY + trSize, trX + trSize / 2 , trY + trSize / 2);
+      
+      
+      parent.text("PASTAGAME SOFTWARE PRESENTS - FALL 2013 KMU GAME PROJECT", parent.width / 2, parent.height + 500 , -1000);
 	}
 }
